6069fc922f47dd7843b23f28ede3e3a426209b27
Brendan <brendanparvin7@gmail.com>
2026-03-17 19:51:36 -0700

    Created ESP-IDF Project

---
+++ components/max_31856_driver/max_31856_driver.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "max_31856_driver.h"
+
+void func(void)
+{
+
+}
---
+++ main/max_31856_driver.c
@@ -0,0 +1,7 @@
+//Defines structs to manage a max 31856 over SPI
+#include <stdatomic.h>
+#include "driver/spi_master.h"
+
+struct max_31856 {
+
+}
